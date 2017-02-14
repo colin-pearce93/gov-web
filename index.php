81b8a236700a417b02dcf1b0fba282ffe0046e4a
ntumin <nicktuminski@gmail.com>
2017-02-14 17:51:42 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Who's Running</title>
+<style type="text/css">
+#pageTop {
+  background:url(style/headerSliverA.png) repeat-x;
+  margin: 0px auto;
+  height: 100px;
+}
+
+#pageMiddle {
+  background-color: #d5e1f3;
+  margin: 0px auto;
+  height: 666px;
+}
+
+</style>
+</head>
+<body>
+
+<div id="pageTop"></div>
+<div id="pageMiddle"></div>
+<div id="pageBottom"></div>
+</body>
+</html>
