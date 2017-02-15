57582e00c211323f61b37e9f198d4ca6b97298ff
Colin Campbell <campbell@Colins-Mac-Pro.local>
2017-02-14 19:42:08 -0500

    stuff and things

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// document.getElementById(id).style.property = new style
+
+
+const load = () => {
+  const apple = "apples";
+  setTimeout(() => {
+    document.getElementById("boobs").style.backgroundColor = "rgba(20, 100, 20, 1)"
+  }, 5000)
+}
+window.onload = load;
